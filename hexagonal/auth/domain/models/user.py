ca38ea227e5600623610d926548aa8904f3cbcf3
Luis Guzman <lumigu.dev@gmail.com>
2025-12-19 01:12:49 -0500

    ✨ Add User model with methods for deactivation and password change

---
+++ hexagonal/auth/domain/models/user.py
@@ -0,0 +1,24 @@
+from dataclasses import field, dataclass
+from datetime import datetime
+from uuid import UUID
+
+
+@dataclass
+class User:
+    id: UUID
+    email: str
+    hashed_password: str
+    is_active: bool = True
+    created_at: datetime = field(default_factory=datetime.now)
+    updated_at: datetime = field(default_factory=datetime.now)
+
+    # Ejemplo de regla de negocio
+    def deactivate(self):
+        if not self.is_active:
+            raise ValueError("Inactive user cannot be deactivated again.")
+        self.is_active = False
+
+    def change_password(self, new_hashed_password: str):
+        if not new_hashed_password:
+            raise ValueError("Password cannot be empty")
+        self.hashed_password = new_hashed_password
