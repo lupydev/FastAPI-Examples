2328c4187b1b8eb51593720ed3c0365a64942d55
Luis Guzman <lumigu.dev@gmail.com>
2025-12-19 02:17:03 -0500

    ✨ Implement FastAPI application with lifespan management and health check endpoint

---
+++ hexagonal/auth/main.py
@@ -0,0 +1,26 @@
+from contextlib import asynccontextmanager
+
+from fastapi import FastAPI
+
+from .infrastructure.config.config import settings
+from .infrastructure.config.db import init_db
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    init_db()
+    yield
+    # shutdown logic here (if needed)
+
+
+app = FastAPI(
+    description="An authentication service using Hexagonal Architecture.",
+    title=settings.PROJECT_NAME,
+    version="0.0.1",
+    lifespan=lifespan,
+)
+
+
+@app.get("/health", tags=["Health"])
+def health_check():
+    return {"status": "ok"}
